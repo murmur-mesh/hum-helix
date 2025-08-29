abc67fe370512ca1f5279a4b0214f124514c9fc9
Andrew Mott <andrew@andrewmott.com>
2025-08-29 12:45:59 -0400

    Create simple fast api server and project script

--- src/hum_helix/server/main.py
+++ src/hum_helix/server/main.py
@@ -1,3 +1,29 @@
 from fastapi import FastAPI
+from faster_whisper import WhisperModel
 
 app = FastAPI()
+
+
+# load a model
+@app.get("/health")
+def health_check():
+    return {"status": "healthy"}
+
+
+# temporarily load whisper for test
+@app.get("/load")
+def load_model():
+    model = WhisperModel("small.en", device="cpu", compute_type="int8")
+    output = model.max_length
+    return {"model.max_length": f"{output}"}
+
+
+# wrap in api, manually load, simply return an echo
+def main():
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+if __name__ == "__main__":
+    main()
