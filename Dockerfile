5f57fd00ea73500f1bea645f2823ede2e15e30d3
Andrew Mott <andrew@andrewmott.com>
2025-08-29 16:07:31 -0400

    Update Dockerfile to run the server

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 
-# CUDA and cuDNN for CTranslate2
+# cuda stuff for ctranslate 2
 FROM nvidia/cuda:12.4.1-cudnn-runtime-ubuntu22.04
 
-# minimal tools + ffmpeg
+# minimal tools + ffmpeg (dont really need ffmpeg for faster-whisper
 RUN apt-get update && apt-get install -y curl ca-certificates ffmpeg && rm -rf /var/lib/apt/lists/*
 
 # uv for python and dependencies
@@ -11,10 +11,16 @@ ENV PATH="/root/.local/bin:${PATH}"
 
 WORKDIR /app
 COPY . . 
+
+EXPOSE 8000
+
 # COPY pyproject.toml uv.lock ./      
 RUN uv sync --frozen --python 3.12 --no-dev || uv sync --python 3.12 --no-dev
-
+RUN uv pip install -e .
 
 
 # run test file
-CMD ["uv", "run", "--python", "3.12", "python", "src/benchmarks/scripts/benchmark.py"]
+# CMD ["uv", "run", "--python", "3.12", "python", "src/benchmarks/scripts/benchmark.py"]
+
+# start the server with pyproject.toml script
+CMD ["uv", "run", "start"]
--- src/hum_helix/server/main.py
+++ src/hum_helix/server/main.py
@@ -1,5 +1,22 @@
 from fastapi import FastAPI
 from faster_whisper import WhisperModel
+from pydantic import BaseModel
+from typing import Optional
+
+# use constr and conint for regex constraits in the future
+
+
+# pydantic hooks amazing into fast api
+class LoadModelRequest(BaseModel):
+    model: str
+    device: str
+    compute_type: str
+    download_root: Optional[str] = None
+
+
+class UnloadModelRequest(BaseModel):
+    model: str
+
 
 app = FastAPI()
 
@@ -7,30 +24,49 @@ app.state.models = {}
 
 
 # simple ping health check
-@app.get("/health")
+@app.get("/ping")
 def health_check():
     return {"status": "healthy"}
 
 
 # simple load model and save to state
-@app.post("/v1/models/{id}:load")
-def load_model(id: str):
-    if app.state.models.get(id) is None:
-        app.state.models[id] = WhisperModel(
-            "small.en", device="cpu", compute_type="int8"
+@app.post("/v1/models/load")
+def load_model(req: LoadModelRequest):
+
+    # if its loaded already then return early
+    if app.state.models.get(req.model) is not None:
+        return {"error": "model already loaded"}
+
+    # otherwise load it and save it to server state
+    print("req.download_root:", req.download_root)
+
+    try:
+        app.state.models[req.model] = WhisperModel(
+            req.model,
+            device=req.device,
+            compute_type=req.compute_type,
+            download_root=req.download_root,
         )
-        if app.state.models.get(id) is None:
-            return {"error": "failed to load model"}
-    return {"model.id": id}
+    except Exception as e:
+        print("Error loading model:", e)
+        return {"error": "failed to load model", "error_message": str(e)}
+
+    return {"model": req.model, "action": "loaded"}
 
 
 # simple unload model and save to state
-@app.post("/v1/models/{id}:unload")
-def unload_model(id: str):
-    if app.state.models.get(id) is not None:
-        del app.state.models[id]
-        return {"model.id": id}
-    return {"error": "model not found"}
+@app.post("/v1/models/unload")
+def unload_model(req: UnloadModelRequest):
+    if app.state.models.get(req.model) is not None:
+        del app.state.models[req.model]
+        return {"model": req.model, "action": "unloaded"}
+    return {"error": "model not loaded"}
+
+
+# simple get models loaded
+@app.get("/v1/models/loaded")
+def list_loaded_models():
+    return {"models": list(app.state.models.keys())}
 
 
 # for starting server with script or directly
